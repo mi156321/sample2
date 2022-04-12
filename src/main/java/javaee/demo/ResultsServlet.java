cc82f8db1d914585535e7b514213a06c0002e9bb
Mikhael Kenzer <choob314@gmail.com>
2022-04-12 00:41:19 -0400

    Corrected a value of @WebServlet

--- src/main/java/javaee/demo/ResultsServlet.java
+++ src/main/java/javaee/demo/ResultsServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ResultsServlet", value = "hiddenservlet/ResultsServlet.do")
+@WebServlet(name = "ResultsServlet", value = "/hiddenservlet/ResultsServlet.do")
 public class ResultsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
